1d48e450489c8efea2fa2a3f6271e9aed2f98ca9
MKhadra <marwa.khadra@mail.utoronto.ca>
2020-03-28 22:26:51 -0400

    Initial round of unit tests added and minor changes to the code made (so that all tests will pass)

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -2116,7 +2116,7 @@ class TestScatter:
         # to minimize the number of image comparison tests
         # we test several aspects of the marker list functionality
         # in one test
-        fig, (ax0, ax1, ax2) = plt.subplots(ncols=3)
+        fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
         
         # test list of length x.size
         ax0.scatter([1, 2, 3], [4, 5, 6], marker=['o', '*', 's'])
@@ -2127,6 +2127,12 @@ class TestScatter:
         # test list with linewidths
         ax2.scatter([1,2,3], [4,5,6], c='red',edgecolors='green',
                     marker=['s', 'o'], linewidths=[10,5],s=500)
+        
+        # test mixes list
+        ax3.scatter([1, 2, 3, 4, 5], [4, 5, 6, 7, 8],
+                    marker=['o', mmarkers.MarkerStyle('o', fillstyle='top'),
+                            '*', mmarkers.MarkerStyle('s', fillstyle='bottom'),
+                            's'])
 
     @image_comparison(baseline_images=['marker_list_of_one'], remove_text=True,
                       extensions=['png'])
@@ -2158,7 +2164,7 @@ class TestScatter:
         fig, ax = plt.subplots()
         with pytest.raises(ValueError) as e:
             ax.scatter([1, 2, 3], [4, 5, 6], marker=[])
-        assert str(e.value) == 'marker cannot be an empty list.'
+        assert str(e.value) == 'marker cannot be an empty list'
 
     # Parameters for *test_scatter_c*. NB: assuming that the
     # scatter plot will have 4 elements. The tuple scheme is:
